a07aef84597a72cd61f08e4a4c4dbf5be20e2b24
Nishan <nisanstha355@gmail.com>
2024-09-20 17:49:37 -0400

    Project Initialized

---
+++ src/main/java/com/project/DateMe/DateMeApplication.java
@@ -0,0 +1,13 @@
+package com.project.DateMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateMeApplication.class, args);
+	}
+
+}
